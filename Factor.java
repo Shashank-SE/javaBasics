de8adb7c2c5d020c95379e6d37904536c0a39819
Shashank Kumar <shashank4260@gmail.com>
2024-06-26 20:24:09 +0530

    first commit

--- Factor.java
+++ Factor.java
@@ -13,13 +13,20 @@ import java.util.Scanner;
 public class Factor {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a natural number: ");
+        System.out.println("Enter a number: ");
         int num = input.nextInt();
-        System.out.print("Factors are: ");
-        for(int i = 1; i <= num; i++) {
-            if(num%i==0) {
-                System.out.print(i + " ");
+        if(num>0) {
+            System.out.print("Factors are: ");
+            System.out.print(1+",");
+
+            for(int i = 2; i <= num/2; i++) {
+                if(num%i==0) {
+                    System.out.print(i + ",");
+                }
             }
+            System.out.println(num);
+        } else {
+            System.out.println("Invalid input....");
         }
         input.close();
     }

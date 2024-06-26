0f280e195fd6899f07a73fb657f8593133be640a
Shashank Kumar <shashank4260@gmail.com>
2024-06-26 19:43:13 +0530

    commit1

--- Factorial.java
+++ Factorial.java
@@ -16,11 +16,11 @@ import java.util.Scanner;
 class Factorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int num = sc.nextInt();
         int factoral = 1;
-        for(int i = 1; i<=num; i++) {
-            factoral = factoral*i;
+        for(int i = 2; i<=num; i++) {
+            factoral *= i;
         }
         System.out.println("Factorial of "+num+" is: "+factoral);
         sc.close();

fd1a463da9966c2c3f61830cf1806ce7b4e7451e
Shashank Kumar <shashank4260@gmail.com>
2024-06-26 19:41:22 +0530

    commit1

---
+++ Factorial.java
@@ -0,0 +1,28 @@
+/*
+Write a Java Program to print the Factorial of the given number.
+Test Case 1: 
+	Input : 3
+	Output : 6
+
+Test Case 2: 
+	Input : 5
+	Output : 120
+
+Test Case 3: 
+	Input : 9
+	Output : 362880
+*/
+import java.util.Scanner;
+class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int num = sc.nextInt();
+        int factoral = 1;
+        for(int i = 1; i<=num; i++) {
+            factoral = factoral*i;
+        }
+        System.out.println("Factorial of "+num+" is: "+factoral);
+        sc.close();
+    }
+}

c868c2c508de1dca23a431a1bbad3ef548c0dac0
shashank-se <shashank4260@gmail.com>
2024-08-23 21:19:35 +0530

    commit-1

--- Pattern5.java
+++ Pattern5.java
@@ -13,7 +13,6 @@ import java.util.Scanner;
 public class Pattern5 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter row = ");
 		int row = sc.nextInt();
 		for(int i = 1; i<=row; i++) {
 			for(int j = 1; j<=row; j++) {
